25291db13576e0dfd55c587710d28976b9174898
conghonglei <conghonglei@gmail.com>
2018-08-09 17:47:35 +0800

    add types

---
+++ types/wire.go
@@ -0,0 +1,12 @@
+package types
+
+import (
+	"github.com/tendermint/go-amino"
+	"github.com/9thchain/blockchain-p2p/crypto"
+)
+
+var cdc = amino.NewCodec()
+
+func init() {
+	crypto.RegisterAmino(cdc)
+}
